f8db029bb8ed20bbc4886b2c091425c4be283366
Study08765 <zstudy025@gmail.com>
2026-07-06 13:49:49 +0530

    Added View Students V2 (Part 1)
    Load student data from Firestore and display in a table with edit and delete options.

---
+++ view_v2.js
@@ -0,0 +1,66 @@
+import { db } from "./firebase.js";
+
+import {
+  collection,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const tbody = document.getElementById("students");
+
+async function loadStudents() {
+
+  tbody.innerHTML = "";
+
+  const snapshot = await getDocs(collection(db, "students_v2"));
+
+  snapshot.forEach((doc) => {
+
+    const s = doc.data();
+
+    tbody.innerHTML += `
+    <tr>
+
+      <td>${s.Roll}</td>
+
+      <td>${s.Name}</td>
+
+      <td>${s.Class}</td>
+
+      <td>${s.ExamType}</td>
+
+      <td>${s.Session}</td>
+
+      <td>
+
+      <button class="edit"
+      onclick="editStudent('${doc.id}')">
+      ✏️ Edit
+      </button>
+
+      <button class="delete"
+      onclick="deleteStudent('${doc.id}')">
+      🗑 Delete
+      </button>
+
+      </td>
+
+    </tr>
+    `;
+
+  });
+
+}
+
+loadStudents();
+
+window.editStudent = function(id){
+
+  alert("Edit : " + id);
+
+}
+
+window.deleteStudent = function(id){
+
+  alert("Delete : " + id);
+
+}
