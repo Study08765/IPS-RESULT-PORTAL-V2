96435cf42763d232b7ab7c2ee53b126e88b7943c
Study08765 <zstudy025@gmail.com>
2026-07-07 22:46:26 +0530

    Implement student result editing and saving functionality
    This file handles the editing and saving of student results, including fetching student data, updating subjects, and calculating grades.

---
+++ edit-result.js
@@ -0,0 +1,123 @@
+import { db } from "./firebase.js";
+
+import {
+  doc,
+  getDoc,
+  updateDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const params = new URLSearchParams(window.location.search);
+const roll = params.get("id");
+
+const name = document.getElementById("name");
+const rollNo = document.getElementById("roll");
+const className = document.getElementById("class");
+const section = document.getElementById("section");
+
+const subjectsDiv = document.getElementById("subjects");
+const saveBtn = document.getElementById("saveBtn");
+
+const snap = await getDoc(doc(db, "students_v2", roll));
+
+if (!snap.exists()) {
+
+  alert("Student Not Found");
+  location.href = "my-students.html";
+
+}
+
+const student = snap.data();
+
+name.innerHTML = student.Name;
+rollNo.innerHTML = student.Roll;
+className.innerHTML = student.Class;
+section.innerHTML = student.Section;
+
+let html = "";
+
+student.Subjects.forEach((sub, index) => {
+
+html += `
+
+<div class="subject">
+
+<label>${sub.name}</label>
+
+<input
+type="number"
+id="m${index}"
+value="${sub.obtained}">
+
+</div>
+
+`;
+
+});
+
+subjectsDiv.innerHTML = html;
+saveBtn.addEventListener("click", async () => {
+
+  const updatedSubjects = [];
+
+  student.Subjects.forEach((sub, index) => {
+
+    updatedSubjects.push({
+      name: sub.name,
+      full: sub.full,
+      obtained: Number(document.getElementById("m" + index).value)
+    });
+
+  });
+
+  let total = 0;
+  let fullTotal = 0;
+
+  updatedSubjects.forEach(sub => {
+    total += sub.obtained;
+    fullTotal += sub.full;
+  });
+
+  const percentage = ((total / fullTotal) * 100).toFixed(2);
+let grade = "";
+let division = "";
+
+if (percentage >= 90) {
+  grade = "A+";
+  division = "First";
+} else if (percentage >= 75) {
+  grade = "A";
+  division = "First";
+} else if (percentage >= 60) {
+  grade = "B";
+  division = "First";
+} else if (percentage >= 45) {
+  grade = "C";
+  division = "Second";
+} else if (percentage >= 33) {
+  grade = "D";
+  division = "Third";
+} else {
+  grade = "F";
+  division = "Fail";
+  }
+  const result =
+    updatedSubjects.some(sub => sub.obtained < 33)
+      ? "FAIL"
+      : "PASS";
+
+  await updateDoc(doc(db, "students_v2", roll), {
+  Subjects: updatedSubjects,
+  Total: total,
+  FullTotal: fullTotal,
+  Percentage: percentage,
+  Grade: grade,
+  Division: division,
+  Result: result,
+  ResultCreated: true
+});
+
+  alert("✅ Result Saved Successfully");
+
+  location.href = "my-students.html";
+
+});
