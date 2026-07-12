834e5d46745732a43b84d32a671a39bf21d74f0b
Study08765 <zstudy025@gmail.com>
2026-07-12 13:11:04 +0530

    Implement timetable management functionality

---
+++ timetable-management.js
@@ -0,0 +1,97 @@
+import { db } from "./firebase.js";
+
+import {
+collection,
+addDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const rows = document.getElementById("rows");
+
+function addRow(type){
+
+const div=document.createElement("div");
+
+div.className="row";
+
+if(type=="Lunch"){
+
+div.innerHTML=`
+
+<select class="type">
+<option>Lunch Break</option>
+</select>
+
+<input class="subject" value="Lunch Break" readonly>
+
+<input class="start">
+
+<input class="end">
+
+`;
+
+}else{
+
+div.innerHTML=`
+
+<select class="type">
+<option>Period</option>
+</select>
+
+<input class="subject" placeholder="Subject">
+
+<input class="start" type="time">
+
+<input class="end" type="time">
+
+`;
+
+}
+
+rows.appendChild(div);
+
+}
+
+document.getElementById("addPeriod").onclick=()=>addRow("Period");
+
+document.getElementById("addLunch").onclick=()=>addRow("Lunch");
+
+document.getElementById("saveBtn").onclick=async()=>{
+
+const cls=document.getElementById("class").value;
+
+if(cls==""){
+alert("Select Class");
+return;
+}
+
+const list=document.querySelectorAll(".row");
+
+for(const r of list){
+
+const type=r.querySelector(".type").value;
+
+const subject=r.querySelector(".subject").value;
+
+const start=r.querySelector(".start").value;
+
+const end=r.querySelector(".end").value;
+
+await addDoc(collection(db,"time_table"),{
+
+Class:cls,
+
+Type:type,
+
+Subject:subject,
+
+StartTime:start,
+
+EndTime:end
+
+});
+
+}
+
+alert("✅ Time Table Saved Successfully");
+
+};
